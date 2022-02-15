45960333a468bc45ae93f6f27f9cd95e4fae2982
lichbroo <97970785+lichbroo@users.noreply.github.com>
2022-02-15 11:22:58 -0500

    arrays

--- loops arrays/app.js
+++ loops arrays/app.js
@@ -1,3 +1,8 @@
-if(true) {
-    console.log("hi");
+let alienDiv = document.getElementById("alien");
+
+let parks = ["Holiday ", "Marott ", "Central ", "Cool Creek "];
+console.log(parks.length);
+
+for(let i = 0; i < 4; i++) {
+    alienDiv.innerHTML += parks[i];
 }
